30052ca035414056ba9e38f59f54d27576c296d8
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-07-21 12:18:17 -0400

    Complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word.lower())
+
+    def match(self, words):
+        return [word for word in words if sorted(word) == self.word]
